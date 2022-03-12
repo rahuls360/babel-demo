19a91a2671a5204bf5fa98f0d299c25a00f66bb0
Rahul Makhija <rahuls360@gmail.com>
2022-03-13 04:28:03 +0530

    add setTimeout edge case, async code, optional chaining

--- src/main.js
+++ src/main.js
@@ -15,3 +15,41 @@ console.log('ES7 Object spread example: ', elvenGauntletsRecipe);
 // ES8 Object.values example
 // Note: Will not transpile without babel/imported polyfills because it is a new method
 console.log('ES8 Object.values example', Object.values(elvenGauntletsRecipe));
+
+// Event queue block scoping example
+// Check babel output to see that `let` isn't simply switched to `var`
+// because the code would not have the same output.
+for (let i = 0; i < 10; i++) {
+  setTimeout(function () {
+    console.log(i);
+  }, 1);
+}
+
+// async/await example from MDN
+function resolveAfter2Seconds() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('resolved');
+    }, 2000);
+  });
+}
+
+async function asyncCall() {
+  console.log('calling');
+  const result = await resolveAfter2Seconds();
+  console.log(result);
+  // expected output: "resolved"
+}
+
+asyncCall();
+
+const person = {
+  name: 'Rahul',
+  age: 27,
+  address: {
+    state: 'karnataka',
+    city: 'bangalore',
+  },
+};
+
+console.log('Optional chaining', person?.address?.city);
