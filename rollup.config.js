ac56fcb7910c7af4a481c1ece34258ce0e9b2dd4
Rahul Makhija <rahuls360@gmail.com>
2022-03-13 05:10:18 +0530

    reduce brotli compression to 7

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import { terser } from 'rollup-plugin-terser';
 import babel from '@rollup/plugin-babel';
 import brotli from 'rollup-plugin-brotli';
 import gzip from 'rollup-plugin-gzip';
+import zlib from 'zlib';
 
 // `npm run build` -> `production` is true
 // `npm run dev` -> `production` is false
@@ -22,6 +23,16 @@ export default {
     babel({ babelHelpers: 'bundled', exclude: 'node_modules/**' }),
     production && terser(), // minify, but only in production
     production && gzip(), // gzip compression
-    production && brotli(), // brotli compression
+    production &&
+      brotli({
+        options: {
+          params: {
+            [zlib.constants.BROTLI_PARAM_MODE]:
+              zlib.constants.BROTLI_MODE_GENERIC,
+            [zlib.constants.BROTLI_PARAM_QUALITY]: 7, // turn down the quality, resulting in a faster compression (default is 11)
+          },
+          // ... see all options https://nodejs.org/api/zlib.html#zlib_class_brotlioptions
+        },
+      }), // brotli compression
   ],
 };
